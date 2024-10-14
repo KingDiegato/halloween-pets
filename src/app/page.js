ab20c66ccd39a8acdc720b865828273da31894f1
diegato <gonzalezbusnego@gmail.com>
2024-10-14 08:26:36 -0300

    create validator and module input

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,37 @@
 "use client";
 import { CldImage, CldUploadWidget } from "next-cloudinary";
-import { useCallback, useState } from "react";
+import { useCallback, useLayoutEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import { fileValidator } from "./utils/fileValidator";
+import { ImageInput } from "@/components/imageInput";
 
 export default function Home() {
   const [binary, setBinary] = useState("");
+  const [error, setError] = useState("");
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.forEach((file) => {
       const reader = new FileReader();
       reader.onabort = () => console.log("file reading was aborted");
       reader.onerror = () => console.log("file reading has failed");
       reader.onload = () => {
-        // Do whatever you want with the file contents
         const binaryStr = reader.result;
         console.log(binaryStr);
         const blob = new Blob([binaryStr], { type: "image/png" });
         const url = URL.createObjectURL(blob);
-
+        setError("");
         setBinary(url);
       };
       reader.readAsArrayBuffer(file);
     });
   }, []);
+
   const { getInputProps, getRootProps, isDragActive } = useDropzone({
     onDrop,
     accept: "image/*",
+    preventDropOnDocument: true,
+    validator: (file) => fileValidator(file, setError),
   });
+  useLayoutEffect(() => {}, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -56,25 +62,24 @@ export default function Home() {
           }}
         </CldUploadWidget> */}
 
-        {binary && <img src={`${binary}`} />}
-
-        <div
-          className="cursor-pointer border-dashed border-amber-500 border-2 rounded-lg p-6 text-center"
-          {...getRootProps()}
-          action="/target"
-          id="target"
-        >
-          <input
-            {...getInputProps()}
-            className="bg-red-500 w-full h-8"
-            placeholder="sono qui"
+        {binary ? (
+          <>
+            <img src={`${binary}`} alt="My-pet" width={600} height={420} />
+            <button
+              className="bg-blue-500 text-white font-bold py-2 px-4 rounded self-end"
+              onClick={() => setBinary("")}
+            >
+              Intentar con otra
+            </button>
+          </>
+        ) : (
+          <ImageInput
+            getRootProps={getRootProps}
+            getInputProps={getInputProps}
+            isDragActive={isDragActive}
           />
-          {isDragActive ? (
-            <p>Suelta la imagen aqui...</p>
-          ) : (
-            <p>Click para subir o intenta soltar una imagen</p>
-          )}
-        </div>
+        )}
+        {error && <p className="text-red-500">{error}</p>}
       </main>
     </div>
   );
