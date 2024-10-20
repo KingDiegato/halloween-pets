f03489d106ecd26b0c277bfff8b1943cf67914b3
diegato <gonzalezbusnego@gmail.com>
2024-10-19 21:19:10 -0300

    feat: added nunito sans

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+const nunitoSans = localFont({
+  src: "./fonts/NunitoSans_10pt-Regular.ttf",
+  variable: "--font-nunito-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nunitoSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
